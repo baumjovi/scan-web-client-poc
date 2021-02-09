0b16b1a4b82648d0fb5841d4bd2fb7ce8cfbbf94
Harry Baum <harry.baum@skybettingandgaming.com>
2021-02-09 12:57:33 +0000

    Created barebones spring boot project

---
+++ src/test/kotlin/io/baum/scanwebclientpoc/ScanWebClientPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.baum.scanwebclientpoc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScanWebClientPocApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
